653c95c2a63dc359032e16f64684c779cccce737
fugang cui <fugang.cui@rea-group.com>
2016-03-13 00:34:36 +0800

    basic feature about to-do list

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,42 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Template.body.helpers({
+    tasks: function () {
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+      //show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .next-task": function(event) {
+      event.preventDefault();
+
+      //Get value from form element
+      var text = event.target.text.value;
+
+      //Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //Clear form
+      event.target.text.value = "";
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  Template.task.events({
+    //set the checked property to the opposite of its current value
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  })
 }
